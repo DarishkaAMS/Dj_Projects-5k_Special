d821d921801f5371579c9f788634ad885441ec5e
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-03 20:34:15 +0300

    Add @admin.register(CoddingSession)

--- codding_session/admin.py
+++ codding_session/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from codding_session.models import CoddingSession
+
 
 # Register your models here.
+
+@admin.register(CoddingSession)
+class CoddingSessionAdmin(ModelAdmin):
+    list_display = ['id', 'name', 'language', 'link', 'starting_time']
