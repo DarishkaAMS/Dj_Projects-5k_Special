3041878ca002aa7f309c55a8ed917c92027fb5f6
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-03 20:46:59 +0300

    Connect static

--- codding_session/urls.py
+++ codding_session/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from codding_session import views
 
 urlpatterns = [
+    path('', views.codding_session_page_view, name="codding_session_page_view"),
     path('home/', views.codding_session_page_view, name="codding_session_page_view")
 ]
